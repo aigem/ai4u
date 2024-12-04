1776e9500ef7bd8d64fdef31f078974115837a04
orztv <qi@outlook.cz>
2024-12-04 21:38:12 +0800

    add F5TTS

--- apps/F5TTS/config/settings.sh
+++ apps/F5TTS/config/settings.sh
@@ -33,10 +33,6 @@ F5TTS_TARGET_DIR="$WORKSPACE_DIR/cache/huggingface/hub/models--SWivid--F5-TTS/sn
 E2TTS_TARGET_DIR="$WORKSPACE_DIR/cache/huggingface/hub/models--SWivid--E2-TTS/snapshots/98016df3e24487aad803aff506335caba8414195/E2TTS_Base"
 WHISPER_TARGET_DIR="$WORKSPACE_DIR/cache/huggingface/hub/models--openai--whisper-large-v3-turbo/snapshots/41f01f3fe87f28c78e2fbf8b568835947dd65ed9/"
 
-# 创建目录（如果不存在）
-mkdir -p "$F5TTS_TARGET_DIR"
-mkdir -p "$E2TTS_TARGET_DIR"
-mkdir -p "$WHISPER_TARGET_DIR"
 # aria2 下载命令
 ARIA2_CMD1="aria2c -c -x 16 -s 16 -k 50M --max-download-limit=50M \
   -d \"$F5TTS_TARGET_DIR\" \
@@ -48,15 +44,3 @@ ARIA2_CMD2="aria2c -c -x 16 -s 16 -k 50M --max-download-limit=50M \
   https://modelscope.cn/models/SWivid/E2-TTS_Emilia-ZH-EN/resolve/master/E2TTS_Base/model_1200000.safetensors"
 
 MODELSCOPE_CMD="modelscope download --model AI-ModelScope/whisper-large-v3-turbo --local_dir \"$WHISPER_TARGET_DIR\""
-
-
-# 测试语音生成命令：
-# 只需在命令行中输入以下命令即可生成语音
-
-```bash
-f5-tts_infer-cli \
---model "F5-TTS" \
---ref_audio "gemini.mp3" \
---ref_text "Gemini 1.5 Pro的主要特点包括突破性的长上下文理解能力能够处理高达100万个Token的上下文信息是上一代模型的30倍以上可以更好地理解复杂的概念和任务. " \
---gen_text "帮我写一篇关于Gemini 1.5 Pro的文章，并理解Gemini 1.5 Pro的主要特点"
-```
